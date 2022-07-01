c79611f947b81cd9c3f5f529f04028fd8ca6d932
Chris Sharp <chris@copmanthorpe.org.uk>
2022-07-01 13:59:18 +0100

    Add files via upload
    Minimal version of an accessory controller. Listens for commands on Address 0001 and turns the builtin LED on and off.

---
+++ DCC_Blink.ino
@@ -0,0 +1,36 @@
+#include <NmraDcc.h>
+NmraDcc  Dcc;
+
+int accyAddr = 1;
+
+// This function is called whenever a normal DCC Turnout Packet is received
+// It returns the Acc Address (1 to 2044) 
+// The Direction 0 (close) or 1 (throw)
+// The Output Power true  (not checking this)
+void notifyDccAccTurnoutOutput(uint16_t Addr, uint8_t Direction, uint8_t OutputPower) {
+  if (Addr == accyAddr)
+  {
+    if (Direction)
+    {
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
+    else
+    {
+      digitalWrite(LED_BUILTIN, LOW);
+    }
+  }
+}
+
+void setup() { 
+  // Setup which External Interrupt, the Pin it's associated with that we're using and enable the Pull-Up 
+  Dcc.pin(0, 2, 1);
+  // Call the main DCC Init function to enable the DCC Receiver
+  Dcc.init(MAN_ID_DIY, 10, CV29_ACCESSORY_DECODER | CV29_OUTPUT_ADDRESS_MODE, 0);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+
+void loop() {
+  Dcc.process();
+}
